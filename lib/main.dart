2e95ae4acf206cf4b60529421a64a383e53b8105
Areeceelsadig <areeceanimates@gmail.com>
2026-07-16 00:01:30 +0300

    Clean up imports in main.dart
    Removed unused imports from main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_6/Signupscreen.dart';
 import 'package:flutter_application_6/app_theme_state.dart';
 import 'package:flutter_application_6/approuter.dart';
-import 'package:flutter_application_6/loginscreen.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'app_theme_cubit.dart';
 
